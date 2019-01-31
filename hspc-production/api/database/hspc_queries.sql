8913b6dc078f46fe501aae0fa1c09e77b25935e7
Joseph Webster <31218426+prometheus1994@users.noreply.github.com>
2019-01-31 17:09:11 -0600

    Update hspc_queries.sql

--- hspc-production/api/database/hspc_queries.sql
+++ hspc-production/api/database/hspc_queries.sql
@@ -1,10 +1,14 @@
-DROP TABLE IF EXISTS Users
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Events;
+DROP TABLE IF EXISTS Teams;
+
 DROP DATABASE IF EXISTS hspc_database
 
 CREATE DATABASE hspc_database
 GO
 
 USE hspc_database;
+
 CREATE TABLE Users(
 	UserID INTEGER PRIMARY KEY IDENTITY(1, 1),
 	TeamID INTEGER,
@@ -16,8 +20,22 @@ CREATE TABLE Users(
 	Email NVARCHAR(45) UNIQUE NOT NULL,
 )
 
-CREATE TABLE Teams
+// Foreign Keys: Level(LevelID), Shool(SchoolID)
+// Tables Referencing Teams: Event(EventID), Submissions(TeamID)
+CREATE TABLE Teams(
+	TeamID INTEGER PRIMARY KEY IDENTITY(1, 1),
+	AdvisorID INTEGER FOREIGN KEY REFERENCES hspc_database.Users(UserID),
+	TeamName NVARCHAR(64),
+	Level INTEGER FOREIGN KEY REFERENCES hspc_database.Level(LevelID),
+	ShoolID INTEGER FOREIGN KEY REFERENCES hspc_database.School(SchoolID)
+)
 
+CREATE TABLE Events(
+	EventID INTEGER PRIMARY KEY IDENTITY(1,1),
+	TeamID INTEGER FOREIGN KEY REFERENCES hspc_database.Teams(TeamID),
+	Location NVARCHAR(64),
+	Date DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET())
+)
 
 -- Change to Account Admin
 USE hspc_database;
