17e2352abcdad11e8383aef6528554f14719c6e0
Daniel Bell <djbell216@gmail.com>
2019-04-01 00:21:05 -0500

    Scoreboard In Progress

--- hspc-production/client/src/components/create-scoreboard.jsx
+++ hspc-production/client/src/components/create-scoreboard.jsx
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
+import BootstrapTable from 'react-bootstrap-table-next';
+import cellEditFactory from 'react-bootstrap-table2-editor';
 import RaisedButton from 'material-ui/RaisedButton';
 import StatusMessages from '../_common/components/status-messages/status-messages';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import '../_common/assets/css/create-scoreboard.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 export default class BoardSetup extends Component {
     constructor(props) {
         super(props)
         this.handleSaveChanges = this.handleSaveChanges.bind(this);
         this.statusMessages = React.createRef();
-        this.beginnerTable = null;
-        this.advancedTable = null;
+        this.row = null;
+        this.columns = null;
+        this.subrows = [];
+        this.subcols = [];
+        this.currentRow = '';
+        this.currentCell = '';
+        this.questionAmount = 5;
         this.state = {
             teamList: [],
             eventList: [],
-            presentTeams: this.props.presentTeams,
+            expanded: [],
+            questionData: []
         }
+    }
+
+    /*
+    * Parses table data as soon as the information arrives.
+    */
+    handleParseTableData = () => {
+        var rawData = this.props.presentTeams;
+        var parsedRows = [];
+        var index = 1;
+        rawData.forEach((row) => {
+            parsedRows.push({
+                index: index,
+                team: row.TeamName,
+                school: row.SchoolName,
+                state: row.StateCode,
+                level: row.QuestionLevel,
+                advisor: row.advisorID,
+                points: 0
+            })
+            index++;
+        })
+        this.rows = parsedRows;
+        this.columns = [
+            { dataField: 'index', text: 'ID' },
+            { dataField: 'team', text: 'Team' },
+            { dataField: 'school', text: 'School' },
+            { dataField: 'state', text: 'State' },
+            { dataField: 'level', text: 'Level' },
+            { dataField: 'advisor', text: 'Advisor' },
+            { dataField: 'points', text: 'Points' }
+        ];
+        this.handleSetQuestions();
+    }
+
+    /*
+    * Handles the row expansion operation.
+    */
+    handleOnExpand = (row, isExpand) => {
+        if (isExpand)
+            this.setState(() => ({ expanded: [...this.state.expanded, row.index] }));
+        else
+            this.setState(() => ({ expanded: this.state.expanded.filter(x => x !== row.index) }));
     }
 
     /*
@@ -25,75 +75,62 @@ export default class BoardSetup extends Component {
     */
     handleSaveChanges() {
         console.log("Changes Saved");
+        console.log(this.subrows, this.subcols);
     }
 
     /*
-    * In Progress
+    * Dynamically sets the number of questions.
     */
-    generateSetupTable = () => {
-        const beginner = [], advanced = [];
-        var bcount = 1, acount = 1;
-        this.state.presentTeams.forEach((team, index) => {
-            if (team.QuestionLevel === "Beginner") {
-                beginner.push(<tr key={index}>
-                    <td>{bcount}</td>
-                    <td>{team.TeamName}</td>
-                    <td>{team.QuestionLevel}</td>
-                </tr>);
-                bcount++;
-            }
-            if (team.QuestionLevel === "Advanced") {
-                advanced.push(<tr key={index}>
-                    <td>{acount}</td>
-                    <td>{team.TeamName}</td>
-                    <td>{team.QuestionLevel}</td>
-                </tr>);
-                acount++;
-            }
-        });
-        this.beginnerTable = <Table striped bordered condensed hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Team Name</th>
-                    <th>Exp Level</th>
-                </tr>
-            </thead>
-            <tbody>
-                {beginner}
-            </tbody>
-        </Table>;
-
-        this.advancedTable = <Table striped bordered condensed hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Team Name</th>
-                    <th>Exp Level</th>
-                </tr>
-            </thead>
-            <tbody>
-                {advanced}
-            </tbody>
-        </Table>;
+    handleSetQuestions = () => {
+        var rows = [];
+        for (let index = 1; index <= this.questionAmount; index++)
+            rows.push({ index: index, answer: "false" })
+        
+        this.subrows.push(rows);
+        this.subcols.push(
+            { dataField: 'index', text: 'Question' },
+            { dataField: 'answer', text: 'Answer' }
+        );
     }
 
     /*
     * Renders the component UI
     */
     render() {
+        const expandRow = {
+            renderer: row => (
+                <div>
+                    <BootstrapTable
+                        keyField='index'
+                        data={this.subrows}
+                        columns={this.subcols}
+                        cellEdit={cellEditFactory({
+                            mode: 'click',
+                            blurToSave: true,
+                            nonEditableRows: () => [0]
+                        })}
+                    />
+                </div>
+            ),
+            expanded: this.state.expanded,
+            onExpand: this.handleOnExpand
+        };
         return (
             <div>
+                {this.handleParseTableData()}
                 <StatusMessages ref={this.statusMessages}></StatusMessages>
-                {this.generateSetupTable()}
-                {this.beginnerTable}
-                {this.advancedTable}
+                <BootstrapTable
+                    id="row-data"
+                    keyField='index'
+                    data={this.rows}
+                    columns={this.columns}
+                    expandRow={expandRow}
+                />
                 <MuiThemeProvider muiTheme={getMuiTheme()}>
                     <div>
                         <RaisedButton
                             className="register-button"
                             label="Save Changes"
-                            style={{ margin: 15 }}
                             backgroundColor={'#00a655'}
                             labelColor={'white'}
                             onClick={(event) => this.handleSaveChanges(event)}
@@ -104,3 +141,4 @@ export default class BoardSetup extends Component {
         )
     }
 }
+
